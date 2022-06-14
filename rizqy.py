70b1d1aec3458030e0d7d569a9fb4b9b9c6ae7d7
anjnrzq <107493375+anjnrzq@users.noreply.github.com>
2022-06-14 23:42:15 +0800

    Add files via upload

---
+++ rizqy.py
@@ -0,0 +1,41 @@
+print("selamat datang di ATM saya")
+print("pilih option")
+print("1. cek uang saya")
+print("2. ambil uang saya")
+print("3. tabung uang saya")
+option=int(input("silahkan pilih option"))
+if option ==  1:
+    print("uang kamu berjumlah Rp.200.000")
+elif option==  2:
+    print("uang kamu berjumlah Rp.200.000, mau ambil berapa?")
+    print("1. Rp.100.000")
+    print("2. Rp.200.000")
+    uang_kamu = 200000
+    option2=int(input("option :"))
+    if option2==  1:
+        hasil=uang_kamu-100000
+        print("uang ka sekarang berjumlah :",hasil1)
+    elif option2==  1:
+        hasil2=uang_kamu-200000
+        print("uang_kamu sekarng berjumlah :",hasil2)
+    else:
+        print("keyword anda salah")
+elif option==  3:
+    uang_kamu=200000
+    print("uang berjumlah Rp.200.000 mau nabung berapa?")
+    option3=int(input("masukkan jumlah uang ;"))
+    hasil3=uang_kamu+option3
+    print("jumlah kamu sekarang adalah",hasil3)
+else:
+    print("keyword anda salah, mohon coba lagi!")
+class yakin:
+        def __init__(self, yakin):
+             self. yakin = yakin
+             
+class yakin:
+       pass
+       ('pilihah Anda Telah Di Akses')
+ 
+print('pilihan Anda Telah Di Akses')
+       
+
